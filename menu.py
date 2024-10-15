f124630a51b76b27a73c0250eced752f16a7311c
limwei97 <168571659+limwei97@users.noreply.github.com>
2024-10-15 22:47:48 +0800

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,20 +1,31 @@
 from entities.customer import Customer
+from entities.baker import Baker
+from entities.cashier import Cashier
+from entities.manager import Manager
+import constant
 
 def main():
-    MANAGER = 1
-    CUSTOMER = 2
-    CASHIER = 3
-    BAKER = 4
+
     while True:
-        print("==Bakery Management System==")
-        print("1.Manager\n2.Customer\n3.Cashier\n4.Baker")
+        print("--- Bakery Management System ---")
+        print("1.Manager\n2.Customer\n3.Cashier\n4.Baker\n5.Exit")
         #validation
         userAction = int(input("Please select action: "))
-        if userAction == CUSTOMER:
+        if userAction == constant.CUSTOMER:
             Customer()
+        elif userAction == constant.BAKER:
+            Baker()
+        elif userAction == constant.MANAGER:
+            Manager()
+        elif userAction == constant.CASHIER:
+            Cashier()
+        elif userAction == constant.EXIT:
+            print("Exiting system. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
         input()
 
-        #control to break from while loop
 
 if __name__ == "__main__":
     main()
